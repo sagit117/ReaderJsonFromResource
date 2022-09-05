44888e54a6c60a06f436185d9ec8f60c78165f4f
Pavel Aks <sagit117@gmail.com>
2022-09-05 23:49:41 +0300

    ReaderJsonFromResource

--- src/main/java/ru/axel/reader/ReaderJsonConfigExample.java
+++ src/main/java/ru/axel/reader/ReaderJsonConfigExample.java
@@ -1,7 +1,28 @@
 package ru.axel.reader;
 
-public class ReaderJsonConfigExample {
-    public static void main(String[] args) {
+import org.json.JSONException;
+import org.json.JSONObject;
 
+import java.io.IOException;
+import java.net.URISyntaxException;
+
+/** Пример чтения json файла из ресурсов */
+public class ReaderJsonConfigExample {
+    /** Пустой конструктор */
+    public ReaderJsonConfigExample() { }
+
+    /**
+     * Точка входа
+     * @param args аргументы вызова
+     */
+    public static void main(String[] args) {
+        try {
+            JSONObject jsonObject = ReaderJsonFromResource.read("./test.json");
+
+            System.out.println(jsonObject.getString("test"));
+            System.out.println(jsonObject.getInt("id"));
+        } catch (URISyntaxException | IOException | JSONException error) {
+            error.printStackTrace();
+        }
     }
 }
--- src/main/java/ru/axel/reader/ReaderJsonFromResource.java
+++ src/main/java/ru/axel/reader/ReaderJsonFromResource.java
@@ -1,22 +1,39 @@
 package ru.axel.reader;
 
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileReader;
-import java.io.IOException;
+import java.io.*;
 import java.net.URISyntaxException;
 import java.net.URL;
 import org.json.*;
 
-public class ReaderJsonConfig {
-    private final String fileName;
+/**
+ * Класс для чтения json файлов из ресурсов
+ */
+public class ReaderJsonFromResource {
+    /** Пустой конструктор */
+    public ReaderJsonFromResource() { }
+    /**
+     * Метод читает json файл из ресурсов и трансформирует его в JSONObject
+     * @param fileNameFromResource имя файла из ресурсов
+     * @return JSONObject
+     * @throws URISyntaxException если путь до файла задан неверно
+     * @throws IOException если произошла ошибка чтения файла
+     */
+    public static JSONObject read(String fileNameFromResource) throws URISyntaxException, IOException {
+        File file = getFileFromResource(fileNameFromResource);
+        StringBuilder stringJson = new StringBuilder((int)file.length());
 
-    public ReaderJsonConfig(String fileNameFromResource) {
-        fileName = fileNameFromResource;
+        return new JSONObject(writeFileToStringBuilder(file, stringJson).toString());
     }
 
-    public JSONObject read() throws URISyntaxException, IOException {
-        URL url = this.getClass().getClassLoader().getResource(fileName);
+    /**
+     * Получаем файл из ресурсов
+     * @param fileName имя файла в ресурсах
+     * @return File
+     * @throws IOException если произошла ошибка чтения файла
+     * @throws URISyntaxException если путь до файла задан неверно
+     */
+    private static File getFileFromResource(String fileName) throws IOException, URISyntaxException {
+        URL url = ReaderJsonFromResource.class.getClassLoader().getResource(fileName);
         File file;
         if (url != null) {
             file = new File(url.toURI());
@@ -24,15 +41,27 @@ public class ReaderJsonConfig {
             throw new IOException("No file exists!");
         }
 
-        StringBuilder stringJson = new StringBuilder((int)file.length());
+        return file;
+    }
 
+    /**
+     * Записываем содержимое файла в StringBuilder
+     * @param file файл
+     * @param stringBuilder StringBuilder
+     * @return StringBuilder
+     * @throws IOException если произошла ошибка чтения файла
+     */
+    private static StringBuilder writeFileToStringBuilder(
+            File file,
+            StringBuilder stringBuilder
+    ) throws IOException {
         try(BufferedReader bufferedReader = new BufferedReader(new FileReader(file))) {
             String s;
             while((s = bufferedReader.readLine()) != null) {
-                stringJson.append(s);
+                stringBuilder.append(s);
             }
 
-            return new JSONObject(stringJson.toString());
+            return stringBuilder;
         }
     }
 }
